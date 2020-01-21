2ad128dcba69a868ef3c896c388e2afe68305911
JessBob <54863103+Jessbob@users.noreply.github.com>
2020-01-20 18:23:17 -0600

    added gifted chat

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,36 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
-// Changes title to the name entered on the start screen
 export default class Chat extends React.Component {
+  state = {
+    messages: []
+  };
+
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any"
+          }
+        }
+      ]
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages)
+    }));
+  }
+
+  // Changes title to the name entered on the start screen
   static navigationOptions = ({ navigation }) => {
     return {
       title: navigation.state.params.name
@@ -20,7 +48,11 @@ export default class Chat extends React.Component {
             backgroundColor: this.props.navigation.state.params.color
           }}
         >
-          <Text style={{ color: "#FFFFFF" }}>This is your chat screen</Text>
+          <GiftedChat
+            messages={this.state.messages}
+            onSend={messages => this.onSend(messages)}
+            user={{ _id: 1 }}
+          />
         </View>
       </View>
     );
